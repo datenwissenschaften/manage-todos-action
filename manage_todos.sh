dd8d91815d4214c9d5fe1346fd3db2abb28710b2
fra-dev-c <extern.martin.franke@cariad.technology>
2024-09-28 18:26:52 +0200

    Initial commit

--- manage_todos.sh
+++ manage_todos.sh
@@ -27,22 +27,48 @@ cat "$TODO_FILE"
 # Loop through each TODO and manage issues
 while IFS= read -r line; do
     FILE=$(echo "$line" | cut -d ':' -f 1)
-    CONTENT=$(echo "$line" | cut -d ':' -f 2-)
+    LINE_NUMBER=$(echo "$line" | cut -d ':' -f 2)
+    CONTENT=$(echo "$line" | cut -d ':' -f 3-)
 
-    echo "Processing TODO: $CONTENT in $FILE"
+    echo "Processing TODO: $CONTENT in $FILE at line $LINE_NUMBER"
 
     # Check if TODO already has an issue number
     if [[ "$CONTENT" =~ \[#([0-9]+)\] ]]; then
         ISSUE_NUMBER=${BASH_REMATCH[1]}
         echo "TODO already linked to issue #$ISSUE_NUMBER."
     else
+        # Extract the title from the first line after TODO
+        TITLE=$(echo "$CONTENT" | sed 's/^[[:space:]]*\/\/\s*TODO:\?//')
+
+        # Find subsequent lines that start with // and treat them as description
+        DESCRIPTION=""
+        NEXT_LINE_NUMBER=$((LINE_NUMBER + 1))
+        while IFS= read -r desc_line; do
+            desc_content=$(echo "$desc_line" | grep "^//" | sed 's/^[[:space:]]*\/\/[[:space:]]*//')
+            if [ ! -z "$desc_content" ]; then
+                DESCRIPTION+="$desc_content"$'\n'
+            else
+                break
+            fi
+            NEXT_LINE_NUMBER=$((NEXT_LINE_NUMBER + 1))
+        done < <(tail -n +$NEXT_LINE_NUMBER "$FILE")
+
+        # Get the author information for the specific TODO line using git blame
+        AUTHOR=$(git blame -L "$LINE_NUMBER","$LINE_NUMBER" "$FILE" --line-porcelain | grep "^author " | cut -d ' ' -f 2-)
+
+        # Append filename and author details to the description
+        DESCRIPTION+="\n**File:** $FILE\n"
+        DESCRIPTION+="**Author:** $AUTHOR\n"
+
         # Create a new GitHub issue for new TODOs
-        echo "Creating a new issue for TODO: $CONTENT"
+        echo "Creating a new issue with title: $TITLE"
+        echo -e "Description: $DESCRIPTION"
+
         ISSUE_RESPONSE=$(curl -X POST \
           -H "Authorization: token $GITHUB_TOKEN" \
           -H "Accept: application/vnd.github.v3+json" \
           https://api.github.com/repos/${GITHUB_REPOSITORY}/issues \
-          -d "{\"title\": \"TODO found in code\", \"body\": \"$CONTENT\", \"labels\": $LABELS_JSON}")
+          -d "{\"title\": \"$TITLE\", \"body\": \"$DESCRIPTION\", \"labels\": $LABELS_JSON}")
 
         ISSUE_NUMBER=$(echo $ISSUE_RESPONSE | jq -r .number)
 
